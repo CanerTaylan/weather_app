54c1ea2394b5a2b7a5467e1f4891a4f3e2f8cc07
Caner Taylan <53885402+CanerTaylan@users.noreply.github.com>
2023-03-17 10:34:58 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import ChangeColor from './Component/Ders/ChangeColor';
+// import Darkmode from './Component/Ders/Darkmode';
+// import Sayiartir from './Component/Ders/Sayiartir';
+// import Ders from './Component/Ders/Ders';
+// import Navbar from './Component/MenuNavBar/Navbar'
+// import TodoList2 from './Component/Todo/Todolist2'
+// import Todolist from './Component/TodoList/Todolist'
+import Trendyol from './Component/Trendyol/Trendyol';
+// import Hava1 from './Component/Weather/Hava1';
+// import Weather from './Component/Weather/Weather';
+
+
+function App() {
+  return (
+    <div>
+     
+      <Trendyol></Trendyol>
+
+    </div>
+  )
+}
+
+export default App
